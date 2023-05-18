a77196346224206c80e1fce5a19b8123a2c1aa64
toleZ <toloyjc@gmail.com>
2023-05-18 19:13:29 -0300

    improvement: asideBar

--- src/common/AutoCard.jsx
+++ src/common/AutoCard.jsx
@@ -1,12 +1,12 @@
 import { FiClock } from "react-icons/fi";
 
 const AutoCard = ({ car }) => {
-  const { model, description, place, price, timeLeft, img } = car;
+  const { model, description, place, price, timeLeft, images } = car;
 
   return (
     <article className="m-2 overflow-hidden rounded-md max-w-sm bg-gray-50">
       <div className="relative">
-        <img src={img} alt={model + "-image"} />
+        <img src={images[0]} alt={model + "-image"} />
         <ul className="m-2 py-1 px-3 absolute bottom-0 left-0 flex gap-4 rounded-md bg-zinc-800">
           <li>
             <span className="flex items-center gap-2 text-white font-semibold">
