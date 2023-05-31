8b3ef8cdb238431e726ce44dd749cb2a3234716c
toleZ <toloyjc@gmail.com>
2023-05-31 20:44:44 -0300

    fix: autInfoList

--- src/common/AutoInfoList.jsx
+++ src/common/AutoInfoList.jsx
@@ -1,17 +1,23 @@
 const formatInfo = (eq) => eq.split(",").map((word) => word.trim());
 
 const AutoInfoList = ({ title, info }) => {
+  const formatedInfo = formatInfo(info);
+
   return (
     <div className="border-y py-6">
       <h3 className="text-lg font-bold">{title}</h3>
 
-      <ul className="mt-3 list-disc">
-        {formatInfo(info)?.map((item) => (
-          <li key={title + item} className="text-md ml-5">
-            {item}
-          </li>
-        ))}
-      </ul>
+      {formatedInfo.length > 1 ? (
+        <ul className="mt-3 list-disc">
+          {formatedInfo?.map((item) => (
+            <li key={title + item} className="text-md ml-5">
+              {item}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p className="text-md mt-3">{info}</p>
+      )}
     </div>
   );
 };
