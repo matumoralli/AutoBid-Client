7b71e601c79fe83fd2d55eaec5cfacba97fb1b6d
Gonzalo Zucca <gonzalozucca_4@outlook.com>
2023-06-07 22:57:26 +0200

    autentificación con facebook ahora con email

--- src/pages/api/auth/[...auth0].js
+++ src/pages/api/auth/[...auth0].js
@@ -5,3 +5,5 @@ export default handleAuth({
     authorizationParams: { scope: "openid profile email" },
   }),
 });
+
+//!El objeto con login y handlelogin NO es necesario para el funcionamiento de la Autenticación, ya que se le está pasando los valores predeterminados. Lo dejo para dar un ejemplo de cómo personalizarla si así se quisiera.
