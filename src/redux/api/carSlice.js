13049f28b82dffb37bb0eab509cbbb8341507396
Gonzalo Zucca <gonzalozucca_4@outlook.com>
2023-07-03 11:34:15 +0200

    fix package.json

--- src/redux/api/carSlice.js
+++ src/redux/api/carSlice.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 // Define a service using a base URL and expected endpoints
 export const carsAPISlice = createApi({
   reducerPath: "carsAPISlice",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000" }),
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.AUTH0_BASE_URL }),
   // transformResponse: (response, meta, arg) =>
   // response.some.deeply.nested.collection,
   endpoints: (builder) => ({
