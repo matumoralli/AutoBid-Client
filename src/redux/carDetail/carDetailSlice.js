b32636cd109c9da129f92f879784b91720731542
Gonzalo Zucca <gonzalozucca_4@outlook.com>
2023-07-12 16:27:44 +0200

    eliminado createCarDetail en carDetailSlice ya que no funciona

--- src/redux/carDetail/carDetailSlice.js
+++ src/redux/carDetail/carDetailSlice.js
@@ -8,46 +8,7 @@ const initialState = {
   carDetail: {},
 };
 
-export const createCarDetail = createAsyncThunk(
-  "carDetail/createCarDetail",
-  async ( formData ) => {
-    console.log(formData);
-    try {
-      const response = await axios.post(
-        "http://localhost:3000/api/carDetailApi",
-        {
-          action: API_ACTIONS.CREATE_CAR_DETAIL,
-          payload: formData,
-        }
-      );
-    } catch (error) {
-      console.log(
-        "There has been a problem with createCarDetail:",
-        error.message
-      );
-    }
-  }
-);
 
-// `{"action":"${API_ACTIONS.CREATE_CAR_DETAIL}", "payload":{
-//   "brand":"${newCarDetail.brand}",
-//   "model":"${newCarDetail.model}",
-//   "year":"${newCarDetail.year}",
-//   "kilometers":"${newCarDetail.kilometers}",
-//   "owner":"${newCarDetail.owner}",
-//   "engine":"${newCarDetail.engine}",
-//   "transmission":"${newCarDetail.transmission}",
-//   "driveTrain":"${newCarDetail.driveTrain}",
-//   "bodyType":"${newCarDetail.bodyType}",
-//   "color":"${newCarDetail.color}",
-//   "highlights":"${newCarDetail.highlights}",
-//   "equipement":"${newCarDetail.equipement}",
-//   "modifications":"${newCarDetail.modifications}",
-//   "knownFlaws":"${newCarDetail.knownFlaws}",
-//   "services":"${newCarDetail.services}",
-//   "addedItems":"${newCarDetail.addedItems}",
-//   "formData":"${formData}",
-//   "email":"${newCarDetail.email}"}}`,
 
 export const carDetailSlice = createSlice({
   name: "carDetail",
